ec33a1c7ae8efded8d59ca34a986d74c61674463
shubham Rawat <rawatshubam269@gmail.com>
2024-06-24 19:59:19 +0530

    RTK Setup

--- convo/src/main.jsx
+++ convo/src/main.jsx
@@ -5,13 +5,18 @@ import routes from "./Routes";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 
+import { Provider } from "react-redux";
+
+import { store } from "./store/store";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <Toaster position="top-right" reverseOrder={false} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <Toaster position="top-right" reverseOrder={false} />
+    </Provider>
   </React.StrictMode>
 );
--- convo/src/pages/home/Home.jsx
+++ convo/src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
 import axios from "axios";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
+
+import { useSelector, useDispatch } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
+import {
+  logout,
+  signInStart,
+  signInSuccess,
+} from "../../store/slices/userSlice";
 
 const Home = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { loading } = useSelector((state) => state.userData);
 
   const handleLogout = async () => {
     const url = `${import.meta.env.VITE_BACKEND_URL}/user/logout`;
@@ -34,10 +44,16 @@ const Home = () => {
     };
 
     try {
+      dispatch(signInStart());
       const res = await axios.get(url, config);
-      console.log("User Details: ", res);
+      // console.log("User Details: ", res);
+      if (res.status === 200) {
+        dispatch(signInSuccess(res.data.data));
+      } else if (res.data.logout) {
+        dispatch(logout());
+      }
     } catch (error) {
-      console.log(error);
+      // console.log(error);
       toast.error(error.response.data.message);
     }
   };
