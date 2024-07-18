ac54a698732e9e7f48cc2dc5c7099ba56914a30b
shubham Rawat <rawatshubam269@gmail.com>
2024-07-18 23:11:13 +0530

    Add vercel file in server app

--- server/index.js
+++ server/index.js
@@ -14,7 +14,13 @@ const { app, server } = require("./socket/index");
 
 const userRoutes = require("./routes/user.route");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookiesParser());
 
