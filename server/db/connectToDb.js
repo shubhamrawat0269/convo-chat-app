f1591ccd298d64ff967c7c882b581fc7132755a0
shubham Rawat <rawatshubam269@gmail.com>
2024-07-20 12:21:03 +0530

    Changes in DB fn

--- server/db/connectToDb.js
+++ server/db/connectToDb.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectToDb = () => {
-  mongoose
-    .connect(`${process.env.DB_URI}`)
-    .then(() => console.log(`Connected to DB at ${process.env.DB_URI}`))
-    .catch((e) => console.log("Failed to connect to DB", e));
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(`${process.env.DB_URI}`);
+    console.log(`Connected to DB at ${process.env.DB_URI}`);
+  } catch (error) {
+    console.log(`Failed to connect to MongoDB`, error);
+  }
 };
 
 module.exports = connectToDb;
